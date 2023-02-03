9505e033f87838f63521e9a0710fb639995e843b
Katsuya Mizuta <k_mizuta@freestyles.jp>
2023-02-04 07:37:22 +0900

    ボール以外のオブジェクトも打てるように修正

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -27,7 +27,7 @@ public class Target : MonoBehaviour
     public void display()
     {
         this.obj.SetActive(true);
-        this.obj.GetComponent<SphereCollider>().enabled = true;
+        this.obj.GetComponent<Collider>().enabled = true;
     }
 
     public void hide()
@@ -74,7 +74,7 @@ public class Target : MonoBehaviour
 
     public void colliderOff()
     {
-        this.obj.GetComponent<SphereCollider>().enabled = false;
+        this.obj.GetComponent<Collider>().enabled = false;
     }
 
     internal bool isSmallPositionZ(float targetPositionZ)
