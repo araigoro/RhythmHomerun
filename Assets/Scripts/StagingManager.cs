c0899478798fc42610278f319e94e7a499e1cce7
DESKTOP-EDJF9MH\kaito <kaito1019baseball@icloud.com>
2023-03-24 21:46:52 +0900

    ・スクリプト修正 ・紙吹雪エフェクト修正

--- Assets/Scripts/StagingManager.cs
+++ Assets/Scripts/StagingManager.cs
@@ -94,8 +94,11 @@ public class StagingManager : MonoBehaviour
     public void SwitchMainCamera()
     {
         //followCamera.CancelFollowTarget();
-        //followVCamera.LookAt = null;
-        followVCamera.gameObject.SetActive(false);
+        if (followVCamera != null)
+        {
+            followVCamera.LookAt = null;
+            followVCamera.gameObject.SetActive(false);
+        }
         buttonSwing.SetActive(true);
         mainVCamera.gameObject.SetActive(true);
     }
